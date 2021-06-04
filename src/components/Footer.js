41be0b13e9cda0e778e26207574decaa697b182c
Stackbit Code Editor <projects@stackbit.com>
2021-06-04 23:18:59 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,13 +46,13 @@ export default class Footer extends React.Component {
                       <ActionLink key={action_idx} {...this.props} action={action} />
                     ))}
                   </div>
-                    <div className="site-info">
+                  <div className="site-info">
                     {htmlToReact(_.get(this.props, 'data.config.footer.content', null))}
                     &nbsp;
                     {_.map(_.get(this.props, 'data.config.footer.links', null), (action, action_idx) => (
                     <ActionLink key={action_idx} {...this.props} action={action} />
                     ))}
-                    </div>
+                  </div>
                 </div>
               </div>
             </footer>
