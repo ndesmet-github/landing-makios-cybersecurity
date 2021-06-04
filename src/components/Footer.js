793121c03da3d08f609817f5c89859d5cfb0ffb9
Stackbit Code Editor <projects@stackbit.com>
2021-06-04 23:46:26 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,6 @@ export default class Footer extends React.Component {
                   )}
                   <div className="site-info">
                       function newContent() {
-      document.open();
       document.write("<h1>Out with the old, in with the new!</h1>");
       document.close();
     }
