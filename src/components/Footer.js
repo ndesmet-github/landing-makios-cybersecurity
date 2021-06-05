e6e12f0c1b915aadfd7156a33158bd029e9a4ea7
Stackbit Code Editor <projects@stackbit.com>
2021-06-05 01:00:57 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,8 @@ export default class Footer extends React.Component {
                   )}
                   <div className="site-info">
 
-                    const element = React.createElement('h1', {classname: 'greeting'});
+                    const element = React.createElement('h1', {classname: 'greeting'}, 
+                    );
                     
                     {htmlToReact(_.get(this.props, 'data.config.footer.content', null))}
                     &nbsp;TESTING
