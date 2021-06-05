c1e4ec78afbe3192c1516c0ed85137d85511ed70
Stackbit Code Editor <projects@stackbit.com>
2021-06-05 00:53:51 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,7 @@ export default class Footer extends React.Component {
                   )}
                   <div className="site-info">
 
-                    const date = {getFullYear()};
+                    const date = {Year()};
                     
                     {htmlToReact(_.get(this.props, 'data.config.footer.content', null))}
                     &nbsp;TESTING
