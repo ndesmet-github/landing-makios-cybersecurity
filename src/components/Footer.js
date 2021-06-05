9986beedeb759da505a25f38483ddb0091bf5f7f
Stackbit Code Editor <projects@stackbit.com>
2021-06-05 00:31:34 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
-import DatePicker from reac
+import DatePicker from react
 
 import components from './index';
 import ActionLink from './ActionLink';
