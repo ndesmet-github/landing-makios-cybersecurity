8e8a6443493467b698e47b92d7c72aaa91991752
Stackbit Code Editor <projects@stackbit.com>
2021-06-05 01:56:40 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,7 +41,7 @@ export default class Footer extends React.Component {
                   )}
                   <div className="site-info">
 
-                    Copyright © 2006-{`${new Date("YYYY").toString()}`}
+                    Copyright © 2006-{`${new Date().toString(YYYY)}`}
                     
                     {htmlToReact(_.get(this.props, 'data.config.footer.content', null))}
                     &nbsp;
