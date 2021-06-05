22c0078a805946b0580d2b19ce8032aa29e953d8
Stackbit Code Editor <projects@stackbit.com>
2021-06-05 00:59:00 +0000

    Footer.js: changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -41,13 +41,12 @@ export default class Footer extends React.Component {
                   )}
                   <div className="site-info">
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger.</h1>;
-}
-
+const element = (
+  <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+);
                     
                     {htmlToReact(_.get(this.props, 'data.config.footer.content', null))}
                     &nbsp;TESTING
